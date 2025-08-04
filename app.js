48a2da575567a57c055746d64a37127a7d08356c
soft42111 <soft4211@email.com>
2025-08-04 19:50:58 +0500

    FIRST COMMIT

---
+++ app.js
@@ -0,0 +1,65 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+
+let turnO = true;
+let gameOver = false;
+
+const winPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (gameOver) return; // do nothing if game already won
+
+    if (turnO) {
+      box.innerText = "O";
+      turnO = false;
+    } else {
+      box.innerText = "X";
+      turnO = true;
+    }
+
+    box.disabled = true;
+    checkWinner();
+  });
+});
+
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+
+    if (pos1Val !== "" && pos1Val === pos2Val && pos2Val === pos3Val) {
+      gameOver = true;
+
+      // Optional: disable all boxes
+      boxes.forEach(box => box.disabled = true);
+
+      // Redirect after short delay
+      setTimeout(() => {
+        window.location.href = 'http://127.0.0.1:5501/WinnerPage/index.html';
+      }, 500); // half-second delay so player sees result
+      break;
+    }
+  }
+};
+
+resetBtn.addEventListener("click", () => {
+  boxes.forEach((box) => {
+    box.innerText = "";
+    box.disabled = false;
+  });
+
+  turnO = true;
+  gameOver = false;
+  document.querySelector("h1").innerText = "The winner is:";
+});
